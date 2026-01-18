4b74cad7e14389b8208bf32c0a0357cee4eb6186
lakshmivelayuthan <lakshmivelayuthan@gmail.com>
2026-01-18 22:09:20 +0530

    Delete SQL/monthly_new_student_registrations.sql

--- SQL/monthly_new_student_registrations.sql
+++
@@ -1,7 +0,0 @@
-/*	Monthly new student registrations */
-SELECT 
-    DATE_FORMAT(join_date, '%Y-%m') AS month,
-    COUNT(*) AS registrations
-FROM Students
-GROUP BY DATE_FORMAT(join_date, '%Y-%m')
-ORDER BY month;

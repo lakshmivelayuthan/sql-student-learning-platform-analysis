9960eff0cc50c64f60b2a83d1ae934ab2fe4be49
lakshmivelayuthan <lakshmivelayuthan@gmail.com>
2026-01-18 17:15:24 +0530

    Update highest_average_completion_percentage_category.sql

--- SQL/highest_average_completion_percentage_category.sql
+++ SQL/highest_average_completion_percentage_category.sql
@@ -1,8 +1,8 @@
 /*	The category that has the highest average completion percentage */
 SELECT 
-    c.category,
-    AVG(p.percentage_completed) AS avg_completion
-FROM Course_Progress p
-JOIN Courses c ON p.course_id = c.course_id
-GROUP BY c.category
+    p.category,
+    AVG(cp.Completion_percentage) AS avg_completion
+FROM Course_Progress cp
+JOIN programs p ON p.Program_id = cp.Program_id
+GROUP BY p.category
 ORDER BY avg_completion DESC;

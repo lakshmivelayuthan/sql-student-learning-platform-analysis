17e4853abfaa4907b7136ed752365b84859da1ee
lakshmivelayuthan <lakshmivelayuthan@gmail.com>
2026-01-18 17:28:27 +0530

    Update inactive_students.sql

--- SQL/inactive_students.sql
+++ SQL/inactive_students.sql
@@ -1,8 +1,21 @@
 /*	Identifying inactive students (not accessed in last 30 days) */
 SELECT 
     s.student_id,
-    s.name,
-    p.last_access_date
-FROM Students s
-JOIN Course_Progress p ON s.student_id = p.student_id
-WHERE p.last_access_date < CURRENT_DATE - INTERVAL 30 DAY;
+    s.student_name,
+    MAX(cp.last_date_accessed) AS last_accessed
+FROM students s
+JOIN course_progress cp 
+    ON s.student_id = cp.student_id
+GROUP BY s.student_id, s.student_name
+HAVING MAX(cp.last_date_accessed) <CURRENT_DATE - INTERVAL 30 DAY;
+
+/* SQL Server */
+SELECT 
+    s.student_id,
+    s.student_name,
+    MAX(cp.last_date_accessed) AS last_accessed
+FROM students s
+JOIN course_progress cp 
+    ON s.student_id = cp.student_id
+GROUP BY s.student_id, s.student_name
+HAVING MAX(cp.last_date_accessed) < DATEADD(DAY, -30, GETDATE());

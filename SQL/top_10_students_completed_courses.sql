a96f473d56fb13835b528fc3d96104c09e7e50d6
lakshmivelayuthan <lakshmivelayuthan@gmail.com>
2026-01-18 17:04:31 +0530

    Update top_10_students_completed_courses.sql

--- SQL/top_10_students_completed_courses.sql
+++ SQL/top_10_students_completed_courses.sql
@@ -1,11 +1,23 @@
 /*	List the top 10 students with the most completed courses. */
+/* Postgre SQL */
 SELECT 
     s.student_id,
-    s.name,
+    s.student_name,
     COUNT(*) AS completed_courses
 FROM Enrolments e
 JOIN Students s ON e.student_id = s.student_id
-WHERE e.status = 'completed'
-GROUP BY s.student_id, s.name
+WHERE e.status_ = 'completed'
+GROUP BY s.student_id, s.student_name
 ORDER BY completed_courses DESC
 LIMIT 10;
+
+/* MY SQL Server */
+SELECT TOP 10
+    s.student_id,
+    s.student_name,
+    COUNT(*) AS completed_courses
+FROM enrolments e
+JOIN Students s ON e.student_id = s.student_id
+WHERE e.Status_ = 'completed'
+GROUP BY s.student_id, s.student_name
+ORDER BY completed_courses DESC;

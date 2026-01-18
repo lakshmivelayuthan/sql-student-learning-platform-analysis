9d1192f35a18fcded7bf7019103d533bf2496fff
lakshmivelayuthan <lakshmivelayuthan@gmail.com>
2026-01-18 22:25:17 +0530

    Update average_completion_percentage.sql

--- SQL/average_completion_percentage.sql
+++ SQL/average_completion_percentage.sql
@@ -1,9 +1,9 @@
 /*	Average completion percentage per student */
 SELECT 
     s.student_id,
-    s.name,
-    AVG(p.percentage_completed) AS avg_progress
+    s.student_name,
+    AVG(cp.completion_percentage) AS avg_progress
 FROM Students s
-JOIN Course_Progress p ON s.student_id = p.student_id
-GROUP BY s.student_id, s.name
+JOIN Course_Progress cp ON s.student_id = cp.student_id
+GROUP BY s.student_id, s.student_name
 ORDER BY avg_progress DESC;

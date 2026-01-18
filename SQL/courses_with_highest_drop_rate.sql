60c5e1ff77aed21d617f6ff1279722893ce90963
lakshmivelayuthan <lakshmivelayuthan@gmail.com>
2026-01-18 18:01:36 +0530

    Update courses_with_highest_drop_rate.sql

--- SQL/courses_with_highest_drop_rate.sql
+++ SQL/courses_with_highest_drop_rate.sql
@@ -1,10 +1,10 @@
 /*	Courses with highest drop rate */
 SELECT 
-    c.title,
-    SUM(CASE WHEN e.status = 'dropped' THEN 1 ELSE 0 END) AS dropped,
+    p.title,
+    SUM(CASE WHEN e.status_ = 'dropped' THEN 1 ELSE 0 END) AS dropped,
     COUNT(*) AS total,
-    (SUM(CASE WHEN e.status = 'dropped' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)) AS drop_rate
+    (SUM(CASE WHEN e.status_ = 'dropped' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)) AS drop_rate
 FROM Enrolments e
-JOIN Courses c ON e.course_id = c.course_id
-GROUP BY c.title
+JOIN Programs p ON e.Program_id = p.Program_id
+GROUP BY p.title
 ORDER BY drop_rate DESC;

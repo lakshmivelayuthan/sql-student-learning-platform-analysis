dc85a3ecc63ae7b36b79316db207bb31e37a3f3e
lakshmivelayuthan <lakshmivelayuthan@gmail.com>
2026-01-18 17:41:56 +0530

    Update average_course_rating.sql

--- SQL/average_course_rating.sql
+++ SQL/average_course_rating.sql
@@ -1,8 +1,8 @@
 /*	Instructor-wise average course rating */
 SELECT 
     i.instructor_name,
-    AVG(c.rating) AS avg_rating
-FROM Courses c
-JOIN Instructors i ON c.instructor_id = i.instructor_id
+    AVG(p.rating) AS avg_rating
+FROM programs p
+JOIN Instructors i ON p.instructor_id = i.instructor_id
 GROUP BY i.instructor_name
 ORDER BY avg_rating DESC;
